72bfe7dab0295119563a4c87429f7dbe8769130d
Dzmitry Trefilau <38854951+dealmasterdee@users.noreply.github.com>
2023-11-02 13:41:38 +0000

    Update 3 files

---
+++ pages/case-studies.vue
@@ -0,0 +1,41 @@
+<template>
+<Welcome :data="casestudiesContent.welcome" />
+  <ImageBoxes
+    :data="casestudiesContent.featured1"
+    withTestimonial
+  />
+  <ImageBoxes
+    :data="casestudiesContent.nonfeatured1"
+  />
+  <ImageBoxes
+    :data="casestudiesContent.featured2"
+    withTestimonial
+  />
+  <ImageBoxes
+    :data="casestudiesContent.nonfeatured2"
+    />
+    <ImageBoxes
+    :data="casestudiesContent.featured3"
+    withTestimonial
+  />
+  <ImageBoxes
+    :data="casestudiesContent.nonfeatured3"
+    withTestimonial
+  />
+  <ImageBoxes
+    :data="casestudiesContent.featured4"
+    withTestimonial
+  />
+  <ImageBoxes
+    :data="casestudiesContent.nonfeatured4"
+    />
+
+<ContactForm :data="casestudiesContent.contactForm" />
+
+</template>
+<script setup lang="ts">
+import casestudiesContent from "../content/case-studies.json";
+
+const store = useContentStore();
+store.setPageContent(casestudiesContent);
+</script>
