87c75f091171260a766981aa4e9cf50dd9852788
Alina Nazarovich <alina.nazarovich@codex-soft.com>
2023-11-09 15:35:24 +0100

    feat: adaptive Footer component

---
+++ components/Footer/FooterMobile/FooterMobile.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="footer">
+    <NuxtLink to="/">
+      <img
+        :src="useImages(footer.logo.src)"
+        :alt="footer.logo.alt"
+        class="footer__logo"
+      />
+    </NuxtLink>
+    <p class="text-small footer__description">
+      <span v-for="text in footer.description">
+        {{ text }}
+      </span>
+    </p>
+    <div class="footer__icons">
+      <a
+        v-for="icon in footer.icons"
+        :href="icon.link"
+      >
+        <img
+          :src="useImages(icon.href)"
+          :alt="icon.alt"
+        />
+      </a>
+    </div>
+    <div class="text-small footer__trademark">
+      <span v-for="text in footer.trademark">
+        {{ text }}
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+const { defaultContent: { footer } } = useContentStore();
+</script>
+
+<style scoped lang="scss" src="./FooterMobile.scss" />
