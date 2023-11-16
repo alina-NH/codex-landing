c1742734baa7e080b9b6ca3ff21168ac3227f0ae
Alina Nazarovich <alina.nazarovich@codex-soft.com>
2023-11-16 13:48:56 +0100

    feat: service scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,5 +32,68 @@ export default defineNuxtConfig({
       path: '~/components',
       pathPrefix: false,
     },
-  ]
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Codex Software Website',
+        },
+      ],
+      script: [
+        {
+          src: 'https://widget.clutch.co/static/js/widget.js',
+          tagPosition: 'bodyClose',
+        },
+        {
+          src: '//js-eu1.hs-scripts.com/139720886.js',
+          id: 'hs-script-loader',
+          async: true,
+          defer: true,
+          type: 'text/javascript',
+          tagPosition: 'bodyClose',
+        },
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=AW-957998787',
+          async: true,
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-957998787');
+          `,
+        },
+        {
+          innerHTML: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K9F2T4P');
+          `,
+        },
+        {
+          type: "text/javascript",
+          src: "https://app.termly.io/embed.min.js",
+          'data-auto-block': "on",
+          'data-website-uuid': "f77b7e8e-94e7-4c10-b63e-749b17f19603",
+        },
+      ],
+      noscript: [
+        {
+          innerHTML: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K9F2T4P"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          `,
+          tagPosition: 'bodyClose',
+        },
+      ],
+    },
+  },
 });
