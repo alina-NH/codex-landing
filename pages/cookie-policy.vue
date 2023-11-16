1d9a84859cbc13ddff47cd45842ea2552e161472
Alina Nazarovich <alina.nazarovich@codex-soft.com>
2023-11-16 11:24:45 +0100

    feat: Privacy and Cookie Policy pages

---
+++ pages/cookie-policy.vue
@@ -0,0 +1,378 @@
+<template>
+  <section class="text-page">
+    <h1 class="heading-2">
+      Cookie Policy
+    </h1>
+    <p class="paragraph--bold">
+      Last updated August 15, 2023
+    </p>
+    <p class="paragraph">
+      This Cookie Policy tells you how Codex Software Solutions Ltd. (hereinafter: “Codex Software”, “We”, “Us”) uses
+      cookies and similar technologies on <a href="https://codex-soft.com" target="_blank" class="link">codex-soft.com</a>
+      (hereinafter: “Website”). We use them to recognize you and make your
+      experience better. You have the right to control how we use them.
+    </p>
+    <p class="paragraph">
+      We reserve the right to change this Cookie Policy without any prior notification to reflect updates to our cookies
+      or for operational, legal, or regulatory reasons. Check this Cookie Policy regularly to stay informed about our
+      use of cookies and related technologies.
+    </p>
+    <h2 class="heading-3">
+      What are cookies?
+    </h2>
+    <p class="paragraph">
+      Cookies are small files that store information on your computer or phone. Websites use cookies to remember your
+      preferences and improve your experience:
+    </p>
+    <ul class="list">
+      <li class="list__item--short">
+        Save your settings, so you don’t have to adjust them as you visit different pages;
+      </li>
+      <li class="list__item--short">
+        Keep your information, so you don't have to re-enter it;
+      </li>
+      <li class="list__item--short">
+        Provide website operators with information on website use and possible improvements.
+      </li>
+    </ul>
+    <p class="paragraph">
+      We use cookies on our website to make it work better and give you a better experience. Some cookies are necessary
+      for the website to work, while others help us learn more about your experience, so we can improve our website, and
+      advertise our services more efficiently.
+    </p>
+    <p class="paragraph">
+      Cookies don't identify you personally and you can control them if you want.
+    </p>
+    <h2 class="heading-3">
+      Cookie Control
+    </h2>
+    <p class="paragraph">
+      You have the right to accept or reject cookies using the Cookie Consent Manager or by changing your browser
+      settings. The types of cookies we use and their purposes are listed in the table below.
+      Please note, that Essential cookies cannot be rejected as they are necessary for the website to work. If you
+      reject cookies, some parts of the website may not work properly.
+    </p>
+    <div style="overflow-x: auto;">
+      <table class="table">
+        <tr>
+          <th>Cookie</th>
+          <th>Service</th>
+          <th>Type</th>
+          <th>Duration</th>
+          <th>Description</th>
+        </tr>
+        <tr>
+          <td>__cf_bm</td>
+          <td>
+            <a href="https://jetpack.com/support/cookies/" target="_blank" class="link" rel="nofollow,noindex">
+              Cloudflare
+            </a>
+          </td>
+          <td>Essential</td>
+          <td>30 min</td>
+          <td>Cloudflare sets the cookie to support Bot Management</td>
+        </tr>
+        <tr>
+          <td>cf_clearance</td>
+          <td>
+            <a href="https://jetpack.com/support/cookies/" target="_blank" class="link" rel="nofollow,noindex">
+              Cloudflare
+            </a>
+          </td>
+          <td>Essential</td>
+          <td>1 year</td>
+          <td>This cookie is set by Cloudflare</td>
+        </tr>
+        <tr>
+          <td>messagesUtk</td>
+          <td>
+            <a href="https://legal.hubspot.com/privacy-policy" target="_blank" class="link" rel="nofollow,noindex">
+              Hubspot
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>5 m 27 d</td>
+          <td>This cookie is used to recognize visitors who use the chatflow tool</td>
+        </tr>
+        <tr>
+          <td>counters.gif</td>
+          <td>
+            <a href="https://legal.hubspot.com/privacy-policy" target="_blank" class="link" rel="nofollow,noindex">
+              Hubspot
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>session</td>
+          <td>This cookie collects information on user preferences and/or interaction with web-campaign content</td>
+        </tr>
+        <tr>
+          <td>_gid</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>24 hours</td>
+          <td>This cookie is used to collect statistical data on website visits</td>
+        </tr>
+        <tr>
+          <td>hubspotutk</td>
+          <td>
+            <a href="https://legal.hubspot.com/privacy-policy" target="_blank" class="link" rel="nofollow,noindex">
+              Hubspot
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>5 months 27 days</td>
+          <td>This cookie is tracks visitors identity and is passed to Hubspot on form submission to deduplicate contacts
+          </td>
+        </tr>
+        <tr>
+          <td>957998787</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>session</td>
+          <td>This cookie is used to track user activities on the website</td>
+        </tr>
+        <tr>
+          <td>_ga_#</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>1 year 1 month 4 days</td>
+          <td>This cookie is installed by Google Analytics to distinguish individual users</td>
+        </tr>
+        <tr>
+          <td>
+            <p>__hssc</p>
+            <p>__hssrc</p>
+          </td>
+          <td>
+            <a href="https://legal.hubspot.com/privacy-policy" target="_blank" class="link" rel="nofollow,noindex">
+              Hubspot
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>session</td>
+          <td>This cookie is installed by Hubspot to determine if a user has changed their browser</td>
+        </tr>
+        <tr>
+          <td>__hstc</td>
+          <td>
+            <a href="https://legal.hubspot.com/privacy-policy" target="_blank" class="link" rel="nofollow,noindex">
+              Hubspot
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>5 months 27 days</td>
+          <td>This cookie is used to track visitors to the website</td>
+        </tr>
+        <tr>
+          <td>_ga</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>1 year 1 month 4 days</td>
+          <td>This cookie is installed by Google Analytics to anonymously track visitor, session, and campaign data.</td>
+        </tr>
+        <tr>
+          <td>_gat#</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>1 minute</td>
+          <td>This cookie regulates the rate of requests from Google Analytics</td>
+        </tr>
+        <tr>
+          <td>__hssc</td>
+          <td>
+            <a href="https://legal.hubspot.com/privacy-policy" target="_blank" class="link" rel="nofollow,noindex">
+              Hubspot
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>30 minutes</td>
+          <td>This cookie is used to track sessions</td>
+        </tr>
+        <tr>
+          <td>_lfa</td>
+          <td>
+            <a href="https://www.leadfeeder.com/privacy-leadfeeder-contacts/" target="_blank" class="link"
+              rel="nofollow,noindex">
+              LeadFeedr
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>1 year</td>
+          <td>This cookie is used to track website visitors</td>
+        </tr>
+        <tr>
+          <td>_lfa_expiry</td>
+          <td>
+            <a href="https://www.leadfeeder.com/privacy-leadfeeder-contacts/" target="_blank" class="link"
+              rel="nofollow,noindex">
+              LeadFeedr
+            </a>
+          </td>
+          <td>Analytics</td>
+          <td>persistent</td>
+          <td>This cookie contains _Ifa cookie expiration information</td>
+        </tr>
+        <tr>
+          <td>test_cookie</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Advertising</td>
+          <td>15 minutes</td>
+          <td>This cookie is tracks browser cookie support</td>
+        </tr>
+        <tr>
+          <td>IDE</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Advertising</td>
+          <td>1.5 years</td>
+          <td>This cookie is used to measure the conversion rate of ads presented to the user.</td>
+        </tr>
+        <tr>
+          <td>_gcl_au</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Advertising</td>
+          <td>3 months</td>
+          <td>This cookie is set by Google AdSense to track advertising efficiency</td>
+        </tr>
+        <tr>
+          <td>ga-audiences</td>
+          <td>
+            <a href="https://policies.google.com/privacy" target="_blank" class="link" rel="nofollow,noindex">
+              Google Analytics
+            </a>
+          </td>
+          <td>Advertising</td>
+          <td>session</td>
+          <td>This cookie is set by Google Ads to re-engage visitors</td>
+        </tr>
+      </table>
+    </div>
+    <p class="paragraph">
+      To control the use of cookies, please use our Cookie Consent Manager or read the section below.
+    </p>
+    <h2 class="heading-3">
+      Other Means of Cookie Control
+    </h2>
+    <p class="paragraph">
+      Different web browsers have different ways of allowing you to decline cookies. To find out more about how to
+      manage cookies on your specific browser, you should refer to the help menu. Here are some instructions for
+      managing cookies on the most commonly used browsers:
+    </p>
+    <ul class="list">
+      <li class="list__item--short">
+        <a href="https://support.google.com/chrome/answer/95647#zippy=%2Callow-or-block-cookies" target="_blank"
+          class="link" rel="nofollow,noindex">
+          Chrome
+        </a>
+      </li>
+      <li class="list__item--short">
+        <a href="https://support.microsoft.com/en-us/windows/delete-and-manage-cookies-168dab11-0753-043d-7c16-ede5947fc64d"
+          target="_blank" class="link" rel="nofollow,noindex">
+          Internet Explorer
+        </a>
+      </li>
+      <li class="list__item--short">
+        <a href="https://support.mozilla.org/en-US/kb/enhanced-tracking-protection-firefox-desktop?redirectslug=enable-and-disable-cookies-website-preferences&redirectlocale=en-US"
+          target="_blank" class="link" rel="nofollow,noindex">
+          Firefox
+        </a>
+      </li>
+      <li class="list__item--short">
+        <a href="https://support.apple.com/en-ie/guide/safari/sfri11471/mac" target="_blank" class="link"
+          rel="nofollow,noindex">
+          Safari
+        </a>
+      </li>
+      <li class="list__item--short">
+        <a href="https://support.microsoft.com/en-us/windows/microsoft-edge-browsing-data-and-privacy-bb8174ba-9d73-dcf2-9b4a-c582b4e640dd"
+          target="_blank" class="link" rel="nofollow,noindex">
+          Edge
+        </a>
+      </li>
+      <li class="list__item--short">
+        <a href="https://help.opera.com/en/latest/web-preferences/" target="_blank" class="link" rel="nofollow,noindex">
+          Opera
+        </a>
+      </li>
+    </ul>
+    <p class="paragraph">
+      Advertising networks often provide an option to opt out of personalized advertising. For further details, please
+      visit:
+    </p>
+    <ul class="list">
+      <li class="list__item--short">
+        <a href="https://optout.aboutads.info/?c=2&lang=EN" target="_blank" class="link" rel="nofollow,noindex">
+          Digital Advertising Alliance
+        </a>
+      </li>
+      <li class="list__item--short">
+        <a href="https://youradchoices.ca/" target="_blank" class="link" rel="nofollow,noindex">
+          Digital Advertising Alliance of Canada
+        </a>
+      </li>
+      <li class="list__item--short">
+        <a href="https://www.youronlinechoices.com/" target="_blank" class="link" rel="nofollow,noindex">
+          European Interactive Digital Advertising Alliance
+        </a>
+      </li>
+    </ul>
+    <h2 class="heading-3">
+      Other Tracking Technologies We Use
+    </h2>
+    <p class="paragraph">
+      We may use technologies similar to cookies, for example, web beacons ("tracking pixels") - small files containing a
+      unique identifier that enables us to recognize when someone has visited our Website or opened an email including
+      them.
+    </p>
+    <p class="paragraph">
+      We use these technologies to identify and monitor the behavioral patterns of users and the sources of a visit
+      (e.g. from an online advertisement, a third-party website, a link in an email) to improve site performance and to
+      measure the success of our marketing campaigns.
+    </p>
+    <h2 class="heading-3">
+      Contact us
+    </h2>
+    <p class="paragraph">
+      If you have any questions about our use of cookies or other technologies, please email us at <a
+        href="mailto:contact@codex-soft.com" class="link">contact@codex-soft.com</a>.
+    </p>
+  </section>
+</template>
+
+<script setup lang="ts">
+import cookiePolicyContent from "../content/cookie-policy.json";
+
+const store = useContentStore();
+store.setPageContent(cookiePolicyContent);
+</script>
