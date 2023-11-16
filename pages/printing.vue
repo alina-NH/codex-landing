76dbb1197983e07dfcbbef83639c1111f4377fc8
dealmasterdee <zmicier.trefilau@codex-soft.com>
2023-11-16 15:09:47 +0100

    minor fixes

--- pages/printing.vue
+++ pages/printing.vue
@@ -17,6 +17,11 @@
     component="IconBox"
     icons-background="transparent"
   />
+  <ContactForm
+    type="simplified"
+    :data="printingContent.contactFormSimplified"
+    component="list"
+  />
   <ImageBoxes
     :data="printingContent.Cases"
     titlePosition="left"
